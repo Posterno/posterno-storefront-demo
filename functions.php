a00d3705fb28f26cce1bc5be5551d6d1fc124411
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-08-19 09:47:40 +0100

    remove elements from header

--- functions.php
+++ functions.php
@@ -8,3 +8,14 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+/**
+ * Remove the product search and cart from the header.
+ */
+add_action(
+	'init',
+	function() {
+		remove_action( 'storefront_header', 'storefront_product_search', 40 );
+		remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+	}
+);
