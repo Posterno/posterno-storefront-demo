eb8a89a76bd857faa0e7aa6da93c523d50ccfd4f
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-08-19 13:01:45 +0100

    change homepage title

--- functions.php
+++ functions.php
@@ -27,7 +27,26 @@ add_action(
 	'wp_enqueue_scripts',
 	function() {
 
+		if ( ! is_page( get_option( 'page_on_front' ) ) ) {
+			return;
+		}
+
 		wp_enqueue_style( 'posterno-demo', get_stylesheet_directory_uri() . '/dist/css/screen.css', false, '1.0.0' );
 
-	}
+	},
+	20
 );
+
+/**
+ * Change the title on the homepage.
+ *
+ * @since 1.0.0
+ */
+function storefront_homepage_header() {
+	edit_post_link( __( 'Edit this section', 'storefront' ), '', '', '', 'button storefront-hero__button-edit' );
+	?>
+	<header class="entry-header">
+		<h1 class="entry-title">Discover great places with Posterno</h1>
+	</header><!-- .entry-header -->
+	<?php
+}
