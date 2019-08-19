91bac42540ca51f1d6938975a8cd25a5596c21cf
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-08-19 09:42:36 +0100

    hello world

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Theme functions file.
+ *
+ * This file is used to bootstrap the theme.
+ *
+ * @package posterno-storefront-demo
+ */
+
+defined( 'ABSPATH' ) || exit;
---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Primary front-end script.
+ *
+ * Primary JavaScript file. Any includes or anything imported should
+ * be filtered through this file and eventually saved back into the
+ * `/dist/js/app.js` file.
+ */
