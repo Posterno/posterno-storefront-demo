b7a2b6733767fb033dee37bd0729f9fdbf7f447b
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-08-24 19:17:39 +0100

    added gtm

--- functions.php
+++ functions.php
@@ -256,3 +256,24 @@ function wpum_prevent_wp_login() {
 	}
 }
 add_action( 'init', 'wpum_prevent_wp_login' );
+
+/**
+ * Add GTM.
+ */
+add_action(
+	'wp_head',
+	function() {
+
+		?>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TS4KKH2');</script>
+<!-- End Google Tag Manager -->
+		<?php
+
+	},
+	100
+);
